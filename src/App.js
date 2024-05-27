46b186f6bc7a21bd883eb056efb7d38c55766fd2
JohnTheCaptain <joao.gomes.work@hotmail.com>
2024-05-27 17:29:21 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Hello
+          APP text change.
         </a>
       </header>
     </div>
