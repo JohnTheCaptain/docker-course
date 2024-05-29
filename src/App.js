bb574b772c835079517dae1786544e1ce6fd89e4
JohnTheCaptain <joao.gomes.work@hotmail.com>
2024-05-29 12:49:50 +0100

    test comit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. App text change. JEEZ
+          Learn React. App text change.
         </a>
       </header>
     </div>
